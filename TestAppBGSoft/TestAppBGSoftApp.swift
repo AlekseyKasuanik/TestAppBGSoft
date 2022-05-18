b3c6109e05f52e6d6886f5dc486d60f157c3d5ce
Alexey kasyanik <375298012802@mail.ru>
2022-05-18 12:46:59 +0300

    Initial Commit

---
+++ TestAppBGSoft/TestAppBGSoftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppBGSoftApp.swift
+//  TestAppBGSoft
+//
+//  Created by Алексей Касьяник on 18.05.22.
+//
+
+import SwiftUI
+
+@main
+struct TestAppBGSoftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
